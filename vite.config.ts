b4d7041e171771c679c449a91e1dacfd1a1d1ff2
matt <mattbunch@protonmail.com>
2026-05-19 17:08:01 +1200

    fix: Correct base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  base: '/digital-rain/',
   plugins: [svelte()],
   resolve: {
     alias: { $lib: '/src/lib' },
